a92999a31cab46f599508beef01e2ddfa51b9cb2
Nattawut_Meesamsen <T.nattawut2020@hotmail.com>
2020-11-14 11:55:36 +0700

    edit table class bootstrap4

--- myproduct.php
+++ myproduct.php
@@ -17,9 +17,12 @@
 
    <div class="container">
       <h1>My Product</h1>
-      <table class="table my-3">
-         <thead>
-            <tr  class="bg-primary text-white">
+      <div class="col">
+         <a href="product-add.php" class="btn btn-success">เพิ่มสินค้า</a>
+      </div>
+      <table class="table table-bordered table-hover my-3">
+         <thead class="thead-dark">
+            <tr  class="">
                <th scope="col">ชื่อสินค้า</th>
                <th scope="col">รายละเอียด</th>
                <th scope="col">เวลาลงขาย</th>
@@ -29,18 +32,24 @@
          </thead>
          <tbody>
             <?php 
-               $query = $db->query("SELECT * FROM 2ndshop.tb_product ORDER BY pd_id DESC"); 
+               $query = $db->prepare("SELECT * FROM 2ndshop.tb_product WHERE user_id = :user_id ORDER BY pd_id DESC"); 
+               $query->execute([':user_id' => $_SESSION['login_id']]);
             ?>
 
             <?php while($row = $query->fetch(PDO::FETCH_ASSOC)) { ?>
                <tr>
-                  <td><?php echo $row['pd_name'] ?></td>
-                  <td><?php echo $row['pd_detail'] ?></td>
-                  <td><?php echo $row['pd_price'] ?></td>
-                  <td><img src="upload/<?php echo $row['pd_img'] ?>" alt="" width="100px" height="100px"></td>
-                  <td>
-                     <a href="product-edit.php" class="btn btn-warning">Edit</a>
-                     <a href="product-edit.php" class="btn btn-danger">delete</a>
+                  <td class=""><?php echo $row['pd_name'] ?></td>
+                  <td class=""><?php echo $row['pd_detail'] ?></td>
+                  <td class=""><?php echo $row['pd_price'] ?></td>
+                  <td class=""><img src="upload/<?php echo $row['pd_img'] ?>" alt="" width="100px" height="100px"></td>
+                  <td class="mx-1">
+                     <div class="row ">
+                        <div class="col d-flex justify-content-around">
+                           <a href="product-edit.php?update_id=<?php echo $row['pd_id'] ?>" class="btn btn-warning">Edit</a>
+                           <a href="product-delete.php?delete_id=<?php echo $row['pd_id'] ?>" class="btn btn-danger">delete</a>
+                     </div>
+                     
+                     
                   </td>
                </tr>
             <?php } ?>
