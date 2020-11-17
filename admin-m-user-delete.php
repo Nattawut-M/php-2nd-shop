e6dd1946c845e37cfbe1960263c54df14f9a8be9
Nattawut_Meesamsen <T.nattawut2020@hotmail.com>
2020-11-17 23:38:07 +0700

    delete user

--- admin-m-user-delete.php
+++ admin-m-user-delete.php
@@ -6,16 +6,11 @@
    
    if (isset($_REQUEST['delete_id'])) {
       try {
-
+         
          $delete_id = $_REQUEST['delete_id'];
-         $query = $db->prepare("SELECT * FROM 2ndshop.tb_product WHERE pd_id = :id");
-         $query->execute([':id' => $delete_id]);
-         $row = $query->fetch(PDO::FETCH_ASSOC);
-         unlink("upload/".$row['pd_img']); // remove file
-
-         $delete_query = $db->prepare("DELETE FROM 2ndshop.tb_product WHERE pd_id = :pd_id");
-         $delete_query->execute([':pd_id'=> $delete_id]);
-         header("refresh:0;admin-m-product-delete.php");
+         $delete_query = $db->prepare("DELETE FROM 2ndshop.tb_users WHERE user_id = :user_id");
+         $delete_query->execute([':user_id'=> $delete_id]);
+         header("refresh:0;admin-m-user.php");
          
       } catch (PDOException $err) {
          echo $err->getMessage();
